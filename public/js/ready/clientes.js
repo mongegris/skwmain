68e4ef6c7acf034f35ef47266bb2acfae919b091
hdgarau@gmail.com <hdgarau@gmail.com>
2019-04-15 12:43:13 -0300

    nada

--- public/js/ready/clientes.js
+++ public/js/ready/clientes.js
@@ -2,18 +2,19 @@ $(document).ready(function(){
     $('table tbody').on( 'click', 'tr', function () {
         $(this).toggleClass('selected');
     } );
-    $('table').before( $('<input type="checkbox" id="select_all">'));
+
+    /*
+    //AGREGAR SELECT ALL. REVISAR LUGAR
+    $('table').before( $('<div><input type="checkbox" id="select_all"></div>'));
 
     $(':checkbox#select_all').click(function(){
         if($(this).prop('checked'))
         {
-            alert('alaal');
             $('table tbody tr').addClass('selected');
         }
         else
         {
-            alert('a');
             $('table tbody tr').removeClass('selected');
         }
-    })
+    })*/
 });
